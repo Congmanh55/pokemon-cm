76afd4f05f43c8d409db330ef873611117017704
Cong Manh <manhatnpt(@gmail.com>
2024-08-05 11:17:07 +0700

    add axios

--- src/pages/HomePages/index.tsx
+++ src/pages/HomePages/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './home.css';
-import PokemonColection from '../../component/PokemonColection';
-import { IPokemon, IPokemons } from '../../types/interface';
+import axios from 'axios'
+import './home.css'
+import PokemonColection from '../../component/PokemonColection'
+import { IPokemon, IPokemons } from '../../types/interface'
 
 export interface Detail {
-  id: number;
-  isOpen: boolean;
+  id: number
+  isOpen: boolean
 }
 const HomePage = () => {
   const [pokemons, setPokemons] = useState<IPokemon[]>([])
@@ -17,32 +17,31 @@ const HomePage = () => {
 
   useEffect(() => {
     const getPokemon = async () => {
-      const res = await axios.get('https://pokeapi.co/api/v2/pokemon');
-      console.log("all",res.data.results)
+      const res = await axios.get('https://pokeapi.co/api/v2/pokemon')
       const pokeData = await Promise.all(
         res.data.results.map(async (pokemon: IPokemons) => {
-          const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-          return poke.data;
+          const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+          return poke.data
         })
-      );
-      setPokemons(pokeData);
+      )
+      setPokemons(pokeData)
     }
     getPokemon()
   }, [])
-  
+
   return (
     <div className='body'>
       <div className='container'>
         <header className='container-header'>
           <button className='header-btn'>Surprise Me!</button>
-          <select name="Sort by" id="" className='header-select'>
-            <option value="Lowest Number">Lowest Number</option>
-            <option value="Highest Number">Highest Number</option>
-            <option value="A-Z">A-Z</option>
-            <option value="Z-A">Z-A</option>
+          <select name='Sort by' id='' className='header-select'>
+            <option value='Lowest Number'>Lowest Number</option>
+            <option value='Highest Number'>Highest Number</option>
+            <option value='A-Z'>A-Z</option>
+            <option value='Z-A'>Z-A</option>
           </select>
         </header>
-        <PokemonColection pokemons={pokemons} viewDetail={viewDetail} setDetail={setDetail}/>
+        <PokemonColection pokemons={pokemons} viewDetail={viewDetail} setDetail={setDetail} />
       </div>
     </div>
   )
